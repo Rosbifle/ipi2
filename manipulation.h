e89b41d8c00a2d23f812dd96729ee427e4607ed7
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-22 17:11:16 +0100

    Update manipulation.h

--- manipulation.h
+++ manipulation.h
@@ -5,3 +5,4 @@ void action_add(tuile t, int x, int y, historique* historique);
 int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique);
 int pose_tuile(grille* g, tuile t, int x, int y);
 int ret_last_tuil(grille* g, historique* historique ,int sz);
+tuile pop(historique* ph);
