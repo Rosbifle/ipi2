63bf3aef42a944290de2e7a3e7717cfd7fd132db
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-22 19:54:48 +0100

    Delete manipulation.h

--- manipulation.h
+++
@@ -1,7 +0,0 @@
-
-int read_terrain(int x, int y);
-void rot_tuile(tuile* t, int orient);
-void action_add(tuile t, int x, int y, historique* historique);
-int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique);
-int pose_tuile(grille* g, tuile t, int x, int y);
-int ret_last_tuil(grille* g, historique* historique ,int sz)
