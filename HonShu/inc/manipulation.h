21892683260ab13dc8bdd4ee48360f46d35f4b41
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-22 20:35:48 +0100

    Update manipulation.h

--- HonShu/inc/manipulation.h
+++ HonShu/inc/manipulation.h
@@ -1,7 +1,6 @@
 
 int read_terrain(int x, int y);
-void rot_tuile(tuile* t, int orient);
-void action_add(tuile t, int x, int y, historique* historique);
-int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique);
+void action_add(tuile t, int x, int y, historique* historique,enum pos rot_a);
+int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique, enum pos rot_a);
 int pose_tuile(grille* g, tuile t, int x, int y);
 int ret_last_tuil(grille* g, historique* historique ,int sz);
