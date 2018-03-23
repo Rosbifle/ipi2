376a98572c2740f498453fe87c4be6c89f31b25c
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-23 18:06:36 +0100

    Add files via upload

--- HonShu/inc/manipulation.h
+++ HonShu/inc/manipulation.h
@@ -11,8 +11,8 @@
  * - int ret_last_tuil(grille* g, historique* historique ,int sz) : ECRIRE
  */
 
-int read_terrain(int x, int y);
+#include"structures.h"
+
+int read_terrain(grille g, int x, int y);
 void action_add(tuile t, int x, int y, historique* historique,enum pos rot_a);
-int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique, enum pos rot_a);
-int pose_tuile(grille* g, tuile t, int x, int y);
 int ret_last_tuil(grille* g, historique* historique ,int sz);
