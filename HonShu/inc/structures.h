801912f9945a084d2cc402deff78805417948eed
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-23 18:10:09 +0100

    Update structures.h

--- HonShu/inc/structures.h
+++ HonShu/inc/structures.h
@@ -64,3 +64,22 @@ void pose_tuile(grille* g, tuile t, int x, int y);
 grille init_grid(int sz);
 bool test_zone_limite(action ac, grille g);
 int village_ville(grille g);
+
+
+/**
+ * \file manipulation.h
+ * 
+ * 5 fonctions
+ * 
+ * - int read_terrain(int x, int y) : ECRIRE
+ * - void action_add(tuile t, int x, int y, historique* historique,enum pos rot_a) : ECRIRE
+ * - int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique, enum pos rot_a) : ECRIRE
+ * - int pose_tuile(grille* g, tuile t, int x, int y) : ECRIRE
+ * - int ret_last_tuil(grille* g, historique* historique ,int sz) : ECRIRE
+ */
+
+#include"structures.h"
+
+int read_terrain(grille g, int x, int y);
+void action_add(tuile t, int x, int y, historique* historique,enum pos rot_a);
+int ret_last_tuil(grille* g, historique* historique ,int sz);
