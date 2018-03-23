2ae77d1cf66069c817f71f056de303c3b2009f83
xiaoyiyi <liziheng1996@gmail.com>
2018-03-23 17:13:02 +0100

    Add files via upload

---
+++ HonShu/inc/jugement.h
@@ -0,0 +1,15 @@
+/**
+ * \file jugement.h
+ * 
+ * 2 fonctions
+ * 
+ * - bool test_zone_limite(action ac, grille g) : ECRIRE 
+ * - int village_ville(grille g) : ECRIRE
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"structures.h"
+
+bool test_zone_limite(action ac, grille g);
+int village_ville(grille g);
