311a2f5f6015c7f687fe1e02528a5befa8ab19dc
Rosbifle <34656010+Rosbifle@users.noreply.github.com>
2018-03-23 19:03:14 +0100

    Add files via upload

--- HonShu/src/structures.c
+++ HonShu/src/structures.c
@@ -1,4 +1,4 @@
-#include "structures.h"
+#include "./../inc/structures.h"
 
 /* prend en argument une taille, renvoie un malloc n*n de char*/
 /**
@@ -18,7 +18,7 @@ grille init_grid(int sz){
             g.grid[i][j]='v';
         }
     }
-    pose_tuile(&g, tuile_random(), (sz-1)/2-1, (sz-1)/2-1);
+    pose_tuile(g, tuile_random(), (sz-1)/2-1, (sz-1)/2-1, rot_0);
     return g;
 }
 
@@ -91,9 +91,10 @@ void add(action a, historique** h){
   \param 
  *\return 
 */
-void pop(historique** h){
+action pop(historique** h){
     action a=(*h)->Play;
     (*h)=(*h)->next;
+    return a;
 }
 
 /* initialise une main de n cartes*/
@@ -114,6 +115,7 @@ hand init_hand(int n){
 }
 
 
+
 /*retourne type de terrains d'une case*/
 /**
  *\brief 
@@ -129,15 +131,13 @@ int read_terrain(grille g, int x, int y){
   \param 
  *\return 
 */
-void action_add(tuile t, int x, int y, historique* historique, enum pos rot_a){
-    action pose;
-    pose.t=t;
-    pose.coord[0]=x;
-    pose.coord[1]=y;
-    pose.next=NULL;
-    pose.orientation=rot_a;
-    historique->next=&pose;
-    return;
+action* action_create(tuile t, int x, int y, enum pos rot_a){
+    action* pose=malloc(sizeof(action));
+    pose->t=t;
+    pose->coord[0]=x;
+    pose->coord[1]=y;
+    pose->orientation=rot_a;
+    return pose;
 }
 
 /**
@@ -145,40 +145,40 @@ void action_add(tuile t, int x, int y, historique* historique, enum pos rot_a){
   \param 
  *\return 
 */
-int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique,enum pose rot_a){
-    if (posable(*g , t, x, y,rot_a)==0){
+int pose_tuile_histo(grille g, tuile t, int x, int y, historique* historique,enum pos rot_a){
+    if (posable(g , t, x, y,rot_a)==0){
         action_add(t, x, y, historique, rot_a);
-        if (rot_a->==rot_0){
-            g->grid[x][y]=t.terrains[0];  
-            g->grid[x][y+1]=t.terrains[1];
-            g->grid[x][y+2]=t.terrains[2];
-            g->grid[x+1][y]=t.terrains[3];
-            g->grid[x+1][y+1]=t.terrains[4];
-            g->grid[x+1][y+2]=t.terrains[5];
+        if (rot_a==rot_0){
+            g.grid[x][y]=t.terrains[0];  
+            g.grid[x][y+1]=t.terrains[1];
+            g.grid[x][y+2]=t.terrains[2];
+            g.grid[x+1][y]=t.terrains[3];
+            g.grid[x+1][y+1]=t.terrains[4];
+            g.grid[x+1][y+2]=t.terrains[5];
         }    
         if (rot_a==rot_180){
-            g->grid[x][y]=t.terrains[5];  
-            g->grid[x][y+1]=t.terrains[4];
-            g->grid[x][y+2]=t.terrains[3];
-            g->grid[x+1][y]=t.terrains[2];
-            g->grid[x+1][y+1]=t.terrains[1];
-            g->grid[x+1][y+2]=t.terrains[0];
+            g.grid[x][y]=t.terrains[5];  
+            g.grid[x][y+1]=t.terrains[4];
+            g.grid[x][y+2]=t.terrains[3];
+            g.grid[x+1][y]=t.terrains[2];
+            g.grid[x+1][y+1]=t.terrains[1];
+            g.grid[x+1][y+2]=t.terrains[0];
         }   
         if (rot_a==rot_90){
-            g->grid[x][y]=t.terrains[3];  
-            g->grid[x][y+1]=t.terrains[0];
-            g->grid[x+1][y]=t.terrains[4];
-            g->grid[x+1][y+1]=t.terrains[1];
-            g->grid[x+2][y]=t.terrains[5];
-            g->grid[x+2][y+1]=t.terrains[2];
+            g.grid[x][y]=t.terrains[3];  
+            g.grid[x][y+1]=t.terrains[0];
+            g.grid[x+1][y]=t.terrains[4];
+            g.grid[x+1][y+1]=t.terrains[1];
+            g.grid[x+2][y]=t.terrains[5];
+            g.grid[x+2][y+1]=t.terrains[2];
         }
         if (rot_a==rot_270){
-            g->grid[x][y]=t.terrains[2];  
-            g->grid[x][y+1]=t.terrains[5];
-            g->grid[x+1][y]=t.terrains[1];
-            g->grid[x+1][y+1]=t.terrains[4];
-            g->grid[x+2][y]=t.terrains[0];
-            g->grid[x+2][y+1]=t.terrains[3];
+            g.grid[x][y]=t.terrains[2];  
+            g.grid[x][y+1]=t.terrains[5];
+            g.grid[x+1][y]=t.terrains[1];
+            g.grid[x+1][y+1]=t.terrains[4];
+            g.grid[x+2][y]=t.terrains[0];
+            g.grid[x+2][y+1]=t.terrains[3];
         }
         return 0;
     }
@@ -190,74 +190,65 @@ int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique,en
   \param 
  *\return 
 */
-int pose_tuile(grille* g, tuile t, int x, int y,enum pose rot_a){
-    if (posable(*g , t, x, y,rot_a)==0){
-        if (rot_a->==rot_0){
-            g->grid[x][y]=t.terrains[0];  
-            g->grid[x][y+1]=t.terrains[1];
-            g->grid[x][y+2]=t.terrains[2];
-            g->grid[x+1][y]=t.terrains[3];
-            g->grid[x+1][y+1]=t.terrains[4];
-            g->grid[x+1][y+2]=t.terrains[5];
+int pose_tuile(grille g, tuile t, int x, int y,enum pos rot_a){
+    if (posable(g , t, x, y,rot_a)==0){
+        if (rot_a==rot_0){
+            g.grid[x][y]=t.terrains[0];  
+            g.grid[x][y+1]=t.terrains[1];
+            g.grid[x][y+2]=t.terrains[2];
+            g.grid[x+1][y]=t.terrains[3];
+            g.grid[x+1][y+1]=t.terrains[4];
+            g.grid[x+1][y+2]=t.terrains[5];
         }    
         if (rot_a==rot_180){
-            g->grid[x][y]=t.terrains[5];  
-            g->grid[x][y+1]=t.terrains[4];
-            g->grid[x][y+2]=t.terrains[3];
-            g->grid[x+1][y]=t.terrains[2];
-            g->grid[x+1][y+1]=t.terrains[1];
-            g->grid[x+1][y+2]=t.terrains[0];
+            g.grid[x][y]=t.terrains[5];  
+            g.grid[x][y+1]=t.terrains[4];
+            g.grid[x][y+2]=t.terrains[3];
+            g.grid[x+1][y]=t.terrains[2];
+            g.grid[x+1][y+1]=t.terrains[1];
+            g.grid[x+1][y+2]=t.terrains[0];
         }   
         if (rot_a==rot_90){
-            g->grid[x][y]=t.terrains[3];  
-            g->grid[x][y+1]=t.terrains[0];
-            g->grid[x+1][y]=t.terrains[4];
-            g->grid[x+1][y+1]=t.terrains[1];
-            g->grid[x+2][y]=t.terrains[5];
-            g->grid[x+2][y+1]=t.terrains[2];
+            g.grid[x][y]=t.terrains[3];  
+            g.grid[x][y+1]=t.terrains[0];
+            g.grid[x+1][y]=t.terrains[4];
+            g.grid[x+1][y+1]=t.terrains[1];
+            g.grid[x+2][y]=t.terrains[5];
+            g.grid[x+2][y+1]=t.terrains[2];
         }
         if (rot_a==rot_270){
-            g->grid[x][y]=t.terrains[2];  
-            g->grid[x][y+1]=t.terrains[5];
-            g->grid[x+1][y]=t.terrains[1];
-            g->grid[x+1][y+1]=t.terrains[4];
-            g->grid[x+2][y]=t.terrains[0];
-            g->grid[x+2][y+1]=t.terrains[3];
+            g.grid[x][y]=t.terrains[2];  
+            g.grid[x][y+1]=t.terrains[5];
+            g.grid[x+1][y]=t.terrains[1];
+            g.grid[x+1][y+1]=t.terrains[4];
+            g.grid[x+2][y]=t.terrains[0];
+            g.grid[x+2][y+1]=t.terrains[3];
         }
         return 0;
     }
     return 1; 
 }
 
-/**
- *\brief 
-  \param 
- *\return 
-*/
-tuile pop(hlist ph){
-    action act=(*ph)->A;
-    *ph=&(*ph->next);
-    return act.t;
-}
 
 /**
  *\brief 
   \param 
  *\return 
 */
-int ret_last_tuil(grille* g, hlist historique ,int sz){
-    if (historique->next==NULL){
+int ret_last_tuil(grille g, hlist hist ,int sz, enum pos rot_a){
+    if (hist->next==NULL){
         return 1;
     }
+    int i;
     for(i=0;i<sz;i++){
-            free(g[i]);
-            free(g);
+        free(g.grid[i]);
     }
-    pop(historique);
-    grille* g= &init_grid_empty(sz);
-    while (historique->next != NULL){
-        pose_tuile(g,historique->Play->t,historique->Play->coord[1],historique->Play->coord[2]);
-        historique=historique->next;
+    free(g.grid);
+    pop(&hist);
+    g=init_grid(sz);
+    while (hist->next != NULL){
+        pose_tuile(g,(hist->Play).t,hist->Play.coord[1],hist->Play.coord[2], rot_a);
+        hist=hist->next;
     }
     return 0;
 }
