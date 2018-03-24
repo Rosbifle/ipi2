dda8331aa7c1f315d7647772c19ab45598050bec
xiaoyiyi <liziheng1996@gmail.com>
2018-03-24 23:40:00 +0100

    Update structures.h

--- HonShu/inc/structures.h
+++ HonShu/inc/structures.h
@@ -148,17 +148,4 @@ int pose_tuile_histo_action(grille g, hlist* histo, action act);
 int ret_last_tuil(grille g, hlist hist ,int sz, enum pos rot_a);
 
 
-/**
- * \file manipulation.h
- * 
- * 5 fonctions
- * 
- * - int read_terrain(int x, int y) : ECRIRE
- * - void action_add(tuile t, int x, int y, historique* historique,enum pos rot_a) : ECRIRE
- * - int pose_tuile_histo(grille* g, tuile t, int x, int y, historique* historique, enum pos rot_a) : ECRIRE
- * - int pose_tuile(grille* g, tuile t, int x, int y) : ECRIRE
- * - int ret_last_tuil(grille* g, historique* historique ,int sz) : ECRIRE
- */
-
-
 
