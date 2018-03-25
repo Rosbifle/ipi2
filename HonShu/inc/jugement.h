a97bd65c536871d8710515dba72ea5acec9c8031
xiaoyiyi <liziheng1996@gmail.com>
2018-03-25 22:50:28 +0200

    Update jugement.h

--- HonShu/inc/jugement.h
+++ HonShu/inc/jugement.h
@@ -6,10 +6,14 @@
  * - bool test_zone_limite(action ac, grille g) : ECRIRE 
  * - int village_ville(grille g) : ECRIRE
  */
-
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
 #include<stdio.h>
 #include<stdlib.h>
 #include"structures.h"
 
 bool test_zone_limite(action ac, grille g);
 int village_ville(grille g);
+bool posable(grille g, tuile t, int x, int y, enum pos rot_a );
+
+#endif
